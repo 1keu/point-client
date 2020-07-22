83aaf71daee508372bcecaad2610354079f7a488
池内翔 <ikeuchishou@01.local>
2020-07-23 05:55:52 +0900

    //

--- js/index.js
+++ js/index.js
@@ -14,3 +14,20 @@ var galleryTop = new Swiper('.tab-contents', {
         swiper: galleryThumbs
     }
 });
+
+var historyThumbs = new Swiper('.history-tab-menu', {
+    slidesPerView: 'auto',
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    slideActiveClass: 'swiper-slide-active'
+});
+historyThumbs.on('tap', function () {
+    var current = historyTop.activeIndex;
+    historyThumbs.slideTo(current, 500, true);
+});
+var historyTop = new Swiper('.history-tab-contents', {
+    autoHeight: true,
+    thumbs: {
+        swiper: historyThumbs
+    }
+});
